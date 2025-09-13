b35668087ac4c0b41bd9cbedb65fc877b86c89d9
Douglas Faria <hfsdouglas@gmail.com>
2025-09-12 22:10:46 -0300

    feat: aplica validação dos dados recebidos

--- routes/auth/login.py
+++ routes/auth/login.py
@@ -1,7 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from marshmallow import ValidationError
+
+from routes.auth.schemas import LoginSchema
 
 auth = Blueprint("auth", __name__, url_prefix="/")
 
 @auth.route('/login', methods=['POST'])
 def login():
-    return "Login route"
+    schema = LoginSchema()
+
+    try:
+        data = schema.load(request.json)
+    except ValidationError as error:
+        return jsonify(error.messages), 400
+    
+    return jsonify({
+        "message": "Seja bem-vindo ao Prioriza!",
+        "token": "seu_token_aqui"
+    }), 200
