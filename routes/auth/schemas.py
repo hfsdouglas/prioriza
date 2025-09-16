a0a8545680e8a678f7c722d47c4d7b54fec83bba
Douglas Faria <hfsdouglas@gmail.com>
2025-09-15 22:03:04 -0300

    feat: cria schema para validação de dados vindo do body da requisição post de signin

--- routes/auth/schemas.py
+++ routes/auth/schemas.py
@@ -3,3 +3,8 @@ from marshmallow import Schema, fields, validate
 class LoginSchema(Schema):
     email = fields.Email()
     password = fields.Str(required=True, validate=validate.Length(min=6))
+
+class SignInSchema(Schema):
+    name = fields.Str(required=True, validate=validate.Length(min=8))
+    email = fields.Email()
+    password = fields.Str(required=True, validate=validate.Length(min=6))
