0c5f59991d96508193c7c39bccf8b0f6d46d04f3
Douglas Faria <hfsdouglas@gmail.com>
2025-09-15 22:00:54 -0300

    fix: uuid

--- models/user.py
+++ models/user.py
@@ -1,10 +1,11 @@
 from sqlalchemy import Column, String, UUID
 from database.db import db
+import uuid
 
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = Column(UUID, primary_key=True, default=db.func.uuid_generate_v4())
+    id = Column(UUID, primary_key=True, default=uuid.uuid4())
     email = Column(String(120), unique=True, nullable=False)
     password = Column(String(128), nullable=False)
     name = Column(String(100), nullable=False)
