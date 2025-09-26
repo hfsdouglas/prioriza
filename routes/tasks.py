214e6c40666465391be2e2da78ca860adda94292
Douglas Faria <hfsdouglas@gmail.com>
2025-09-26 15:29:10 -0300

    feat: cria rota para criação de tarefas

--- routes/tasks.py
+++ routes/tasks.py
@@ -1,11 +1,42 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required
+from marshmallow import ValidationError
+
+from database.db import db
+
+from schemas.task import CreateTaskSchema
+
+from models.user import User
+from models.tasks import Task
 
 tasks_bp = Blueprint("tasks", __name__)
 
 @tasks_bp.post("/tasks")
 @jwt_required()
 def create_task():
-    return jsonify({
-        "message": "foo"
-    })
+    schema = CreateTaskSchema()
+
+    try:
+        data = schema.load(request.json)
+
+        user = User.query.filter_by(id=data['user_id']).first()
+
+        if user:
+            task = Task(
+                task = data['task'],
+                user_id = data['user_id']
+            )
+
+            db.session.add(task)
+            db.session.commit()
+
+            return jsonify({
+                "message": "Usuário cadastrado com sucesso!"
+            })
+        else:
+            return jsonify({
+                "message": "Usuário não encontrado!"
+            }), 400
+
+    except ValidationError as error:
+        return jsonify(error.messages)
