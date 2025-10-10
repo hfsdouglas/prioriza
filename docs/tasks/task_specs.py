77d74bec4b940a21627824de77df45df05079e1b
Douglas Faria <hfsdouglas@gmail.com>
2025-10-10 20:20:43 -0300

    feat: cria docs para criação de novas tarefas

--- docs/tasks/task_specs.py
+++ docs/tasks/task_specs.py
@@ -1,5 +1,46 @@
 class Task_Specs():
     def create_task_specs():
         return {
-            
+            'parameters': [
+                {
+                    'name': 'Nova tarefa',
+                    'in': 'body',
+                    'required': True,
+                    'schema': {
+                        'type': 'object',
+                        'properties': {
+                            'task': {
+                                'type': 'string',
+                                'example': 'Eu tenho que fazer alguma coisa, qualquer coisa.'
+                            }
+                        }
+                    }
+                }
+            ], 
+            'responses': {
+                201: {
+                    'description': 'Caso a tarefa seja criada, retorna mensagem de sucesso.',
+                    'examples': {
+                        'application/json': {
+                            'message': 'Tarefa cadastrada com sucesso!'
+                        }
+                    }
+                },
+                400: {
+                    'description': 'Caso os dados estejam inválidos, retorna lista de erros.',
+                    'examples': {
+                        'application/json': {
+                            'errors': {
+                                'task': [
+                                    'Missing data for required field.',
+                                    'Shorter than minimum length 10.'
+                                ]
+                            }
+                        }
+                    }
+                },
+            }
         }
+    
+    
+    
